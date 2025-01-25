39fd2b024d670eecb5130df646fd0193ae8492d7
Jose Luis Rojas Gomez <amarillo17cyl@gmail.com>
2025-01-24 20:35:43 -0600

    It was implemented the functionality to validate the token.

---
+++ auth/auth.go
@@ -0,0 +1,74 @@
+package auth
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
+
+type Token struct {
+	Sub       string
+	Event_Id  string
+	Token_use string
+	Scope     string
+	Auth_time int
+	Iss       string
+	Exp       int
+	Iat       int
+	Client_id string
+	Username  string
+}
+
+// This funci vaidate the token. If the token is not valid
+func Validate(token string) (
+	bool,
+	error,
+	string,
+) {
+
+	// First part contains the payload
+	// Second part contains the headers
+	// Third part contains the sign
+	tokenItems := strings.Split(token, ".")
+
+	if len(tokenItems) != 3 {
+		message := "The token is not valid."
+		fmt.Println(message)
+		return false, nil, message
+	}
+
+	userInfo, err := base64.StdEncoding.DecodeString(tokenItems[1])
+
+	if err != nil {
+		errorMessage := err.Error()
+		fmt.Println("It was an error to decode (base64) the token:", errorMessage)
+		return false, err, errorMessage
+	}
+
+	var t Token
+	err = json.Unmarshal(userInfo, &t)
+
+	if err != nil {
+		errorMessage := err.Error()
+		fmt.Println("It was possible to create an Token struct. It was an error:", errorMessage)
+
+		return false, err, errorMessage
+	}
+
+	// Check if the token is not expired
+	now := time.Now()
+	tm := time.Unix(int64(t.Exp), 0)
+
+	// Token expired if tm is before now
+	if tm.Before(now) {
+		errorMessage := "Token has expired."
+		fmt.Printf("%s Date token is: %s and today: %s", errorMessage, tm.String(), now.String())
+
+		return false, err, errorMessage
+	}
+
+	return true, nil, string(t.Username)
+
+}
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,9 +5,11 @@ import (
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
+
+	"github.com/luisrojas17/ecommerce/auth"
 )
 
-// Returns the HTTP status code and message
+// Returns the HTTP status code and message to describe the error.
 func Handler(path string, method string, body string, headers map[string]string, request events.APIGatewayV2HTTPRequest) (int, string) {
 
 	fmt.Println("Processing path: " + path + " for method: " + method)
@@ -17,6 +19,102 @@ func Handler(path string, method string, body string, headers map[string]string,
 	// Convert the user id and product id. The product id only has to be number.
 	idn, _ := strconv.Atoi(id)
 
-	return 400, "Method Invalid"
+	// Check if token is valid and it had not expired.
+	ok, statusCode, user := authorize(path, method, body, headers)
 
+	if !ok {
+		return statusCode, user
+	}
+
+	switch path[0:4] {
+
+	case "user":
+		return Users(body, path, method, user, id, request)
+
+	case "prod":
+		return Products(body, path, method, user, idn, request)
+
+	case "stoc":
+		return Stock(body, path, method, user, idn, request)
+
+	case "addr":
+		return Address(body, path, method, user, idn, request)
+
+	case "cate":
+		return Categories(body, path, method, user, idn, request)
+
+	case "orde":
+		return Orders(body, path, method, user, idn, request)
+	}
+
+	return 400, "Method Invalid."
+
+}
+
+func authorize(path string, method string, body string, headers map[string]string) (bool, int, string) {
+
+	// For this options the API does not have to validate the token.
+	if (path == "product" && method == "GET") ||
+		(path == "category" && method == "GET") {
+
+		return true, 200, ""
+	}
+
+	// Get the token from headers.
+	token := headers["authorization"]
+	if len(token) == 0 {
+		return false, 401, "The token is mandatory to authenticate the request.s"
+	}
+
+	// Validate token.
+	ok, err, msg := auth.Validate(token)
+
+	if !ok {
+		if err != nil {
+			errorMsg := err.Error()
+
+			fmt.Println("Error", errorMsg)
+
+			return false, 401, errorMsg
+
+		} else {
+			fmt.Println("Token error: ", msg)
+
+		}
+	}
+
+	fmt.Println("Token Ok.")
+
+	return true, 200, msg
+
+}
+
+func Users(body string, path string, method string, user string, id string, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func Products(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func Categories(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func Stock(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func Address(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
+}
+
+func Orders(body string, path string, method string, user string, id int, request events.APIGatewayV2HTTPRequest) (int, string) {
+
+	return 400, "Method Invalid"
 }
