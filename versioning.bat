faedbf2cc4021c57b850c3ca6c8d6911dcec1d28
José Luis Rojas Gómez <amarillo17cyl@gmail.com>
2025-02-07 18:47:29 -0600

    There were added comments to bash scripts.

--- versioning.bat
+++ versioning.bat
@@ -1,3 +1,6 @@
+REM This file wraps all operations like git add, git commit, git push in order to send 
+REM all local changes from local repository to remote repository.
+
 git add .
 git commit -m %1
 git push
--- versioning.sh
+++ versioning.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# This file wraps all operations like git add, git commit, git push in order to send 
+# all local changes from local repository to remote repository.
+
 git add .
 git commit -m $1
 git push
